e892ba063af5599ecd38c07ac89ab84250b60a9e
tunnckoCore <mameto_100@mail.bg>
2015-08-14 09:19:02 +0300

    use `parse-github-url` instead

--- index.js
+++ index.js
@@ -6,8 +6,9 @@
  */
 
 var url = require('url');
-var chalk = require('chalk');
+var red = require('ansi-red');
 var origin = require('remote-origin-url');
+var parse = require('parse-github-url');
 
 /**
  * Expose `username`
@@ -20,22 +21,14 @@ module.exports = username;
  */
 
 function username(cwd, verbose) {
-  var repo = origin.sync(cwd);
-  if (!repo && verbose) {
-    console.error(chalk.red('  Can\'t calculate git-username, which probably means that\n  a git remote origin has not been defined.'));
+  var url = origin.sync(cwd);
+  if (!url && verbose) {
+    console.error(red('  Can\'t calculate git-username, which probably means that\n  a git remote origin has not been defined.'))
+  }
+  if (!url) {
+    return null
   }
 
-  if (!repo) {
-    return null;
-  }
-
-  var o = url.parse(repo);
-  var path = o.path;
-
-  if (path.length && path.charAt(0) === '/') {
-    path = path.slice(1);
-  }
-
-  path = path.split('/')[0];
-  return path;
+  var json = parse(url)
+  return json ? json.user : null;
 }
